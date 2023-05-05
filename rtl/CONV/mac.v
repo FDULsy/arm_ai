f67aee62e5fb64bf39592cdda0e672b5c788ad67
FDULsy <88324239+FDULsy@users.noreply.github.com>
2023-05-05 18:08:46 +0800

    Update mac.v

--- rtl/CONV/mac.v
+++ rtl/CONV/mac.v
@@ -1,5 +1,5 @@
 //first last valid信号没写
-module mac #(parameter DW=8,WW=8,CW=16,ROW=8,COLUMN=6,OW=2*DW+ROW
+module mac #(parameter DW=8,WW=8,CW=21,ROW=8,COLUMN=6,OW=2*DW+5
 ) (
     input [ROW*DW-1:0]         mac_m_data,
     input                      mac_m_first,
@@ -10,11 +10,11 @@ module mac #(parameter DW=8,WW=8,CW=16,ROW=8,COLUMN=6,OW=2*DW+ROW
     input [   COLUMN-1 : 0]    w_en,
     input [COLUMN*CW-1 : 0]    ci,
 
-    output [COLUMN*OW-1 : 0] mac_s_data,
-    output                   mac_s_first,
-    output                   mac_s_last,
-    output                   mac_s_valid,
-    input                    mac_s_ready,
+    output [COLUMN*OW-1 : 0]   mac_s_data,
+    output                     mac_s_first,
+    output                     mac_s_last,
+    output                     mac_s_valid,
+    input                      mac_s_ready,
 
     input clk,
     input rst_n
@@ -25,14 +25,14 @@ wire [   COLUMN-1 : 0] w_en_pass [ROW-1 : 0];
 reg  [COLUMN*WW-1 : 0] w_pass_r [ROW-2 : 0];
 reg  [   COLUMN-1 : 0] w_en_pass_r [ROW-2 : 0];
 
-wire [COLUMN*(CW+1)-1 : 0] co0;
-wire [COLUMN*(CW+2)-1 : 0] co1;
-wire [COLUMN*(CW+3)-1 : 0] co2;
-wire [COLUMN*(CW+4)-1 : 0] co3;
-wire [COLUMN*(CW+5)-1 : 0] co4;
-wire [COLUMN*(CW+6)-1 : 0] co5;
-wire [COLUMN*(CW+7)-1 : 0] co6;
-wire [COLUMN*(CW+8)-1 : 0] co7;
+wire [COLUMN*(CW)-1 : 0] co0;
+wire [COLUMN*(CW)-1 : 0] co1;
+wire [COLUMN*(CW)-1 : 0] co2;
+wire [COLUMN*(CW)-1 : 0] co3;
+wire [COLUMN*(CW)-1 : 0] co4;
+wire [COLUMN*(CW)-1 : 0] co5;
+wire [COLUMN*(CW)-1 : 0] co6;
+wire [COLUMN*(CW)-1 : 0] co7;
 
 assign w_pass[0] = w;
 assign w_en_pass[0] = w_en; 
@@ -54,10 +54,11 @@ generate
                 w_pass_r[i] <= w_pass[i];
                 w_en_pass_r[i] <= w_en_pass[i];
         end
+    end
 endgenerate
 
 //row0
-mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row0(
+mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN)) i_mac_row0(
     .xi(mac_m_data[0*DW +: DW]),
     .wi(w_pass[0]),
     .ci(ci),
@@ -69,7 +70,7 @@ mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row0(
 );
 
 //row1
-mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row1(
+mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN)) i_mac_row1(
     .xi(mac_m_data[1*DW +: DW]),
     .wi(w_pass[1]),
     .ci(co0),
@@ -81,7 +82,7 @@ mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row1(
 );
 
 //row2
-mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row2(
+mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN)) i_mac_row2(
     .xi(mac_m_data[2*DW +: DW]),
     .wi(w_pass[2]),
     .ci(co1),
@@ -93,7 +94,7 @@ mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row2(
 );
 
 //row3
-mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row3(
+mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN)) i_mac_row3(
     .xi(mac_m_data[3*DW +: DW]),
     .wi(w_pass[3]),
     .ci(co2),
@@ -105,7 +106,7 @@ mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row3(
 );
 
 //row4
-mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row4(
+mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN)) i_mac_row4(
     .xi(mac_m_data[4*DW +: DW]),
     .wi(w_pass[4]),
     .ci(co3),
@@ -117,7 +118,7 @@ mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row4(
 );
 
 //row5
-mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row5(
+mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN)) i_mac_row5(
     .xi(mac_m_data[5*DW +: DW]),
     .wi(w_pass[5]),
     .ci(co4),
@@ -129,7 +130,7 @@ mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row5(
 );
 
 //row6
-mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row6(
+mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN)) i_mac_row6(
     .xi(mac_m_data[6*DW +: DW]),
     .wi(w_pass[6]),
     .ci(co5),
@@ -141,7 +142,7 @@ mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row6(
 );
 
 //row7
-mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN))) i_mac_row7(
+mac_row #(.DW(DW),.WW(WW),.CW(CW),.COLUMN(COLUMN)) i_mac_row7(
     .xi(mac_m_data[7*DW +: DW]),
     .wi(w_pass[7]),
     .ci(co6),
