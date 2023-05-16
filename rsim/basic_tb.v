af884957344e1ffdd18e9ec00acafb79009602f1
FDULsy <88324239+FDULsy@users.noreply.github.com>
2023-05-16 19:27:07 +0800

    Create basic_tb.v

---
+++ rsim/basic_tb.v
@@ -0,0 +1,45 @@
+`timescale 1ps/1ps
+module  #(
+    parameter AW= , DW= , DN=  
+);
+
+reg clk,rst_n;
+
+
+//=====例化=====
+
+
+    (
+
+);
+
+
+//==============
+initial begin
+    clk=0;
+    forever begin
+       #5 clk=~clk; 
+    end 
+end
+
+initial begin
+    rst_n=0;
+    #10 rst_n=1;
+    #1000 ;
+    $stop;
+end
+
+initial begin
+    //input inital
+
+    //
+    #16
+
+
+
+
+    //
+
+end
+
+endmodule
