38dcea4484b63b5c367b85d949974216ed01da43
LSY <2417312841@qq.com>
2023-07-09 00:22:58 +0800

    1

--- rtl/CONV/conv_inst_loop.v
+++ rtl/CONV/conv_inst_loop.v
@@ -1,5 +1,5 @@
 module conv_inst_loop #(
-    parameter IRW=30,IN=3   
+    parameter IRW=30,IN=3,AW=14   
 ) (
     input       [IRW*IN-1 : 0]  m_inst      ,
     input                       m_valid     ,
@@ -16,6 +16,36 @@ module conv_inst_loop #(
 
 wire fc;
 assign fc= m_inst[0];
+wire [AW-1 : 0] inst_base;
+wire [   6 : 0] inst_dim0_size;
+wire [AW-1 : 0] out_base;
+wire [AW-1 : 0] next_base;
+reg  [   6 : 0] base_add1 ;
+reg  [   6 : 0] base_add2 ;
+
+
+always @(*) begin
+    case(cnt)
+        0: base_add1 <= 0;
+    endcase
+end
+
+always @(*) begin
+    case(cnt)
+        0: base_add2 <= 0;
+        1: base_add2 <= 7'b0000001;
+        2: base_add2 <= 7'b0000001;
+        3: base_add2 <= inst_dim0_size;
+        4: base_add2 <= 7'b0000001;
+        5: base_add2 <= 7'b0000001;
+        6: base_add2 <= inst_dim0_size;
+        7: base_add2 <= 7'b0000001;
+        8: base_add2 <= 7'b0000001;
+        default: base_add2 <= 0;
+    endcase
+end
+
+assign next_base = 
 
 
 reg    [3:0] cnt    ;
@@ -30,7 +60,7 @@ always @(posedge clk or negedge rst_n) begin
         m1_vlaid <= 1'b0;
     else if(m_valid && m_ready)
         m1_vlaid <= 1'b1;
-    else if((cnt != 4'b0) && s_ready )
+    else if((cnt != 4'b0) && fc )
         m1_vlaid <= 1'b1;
     else
         m1_vlaid <= 0; 
@@ -42,8 +72,10 @@ always @(posedge clk or negedge rst_n) begin
     else if(m_valid && m_ready && fc)
         cnt <= 4'b9;
     else if(s_ready && fc)
-        cnt <= cnt - 1'b;
-    else 
+        cnt <= cnt - 1'b1;
+    else if(!fc)
+        cnt <= 0;
+    else
         cnt <= cnt;
 end
 
